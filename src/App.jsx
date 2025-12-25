304dd9cebe570000304cc95fc0c1416f3f48f400
jiwoon3370 <jiwoon3370@example.com>
2025-12-25 23:22:58 +0900

    React Router세팅, 페이지 라우팅

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from "./routes";
+
+function App() {
+  return (
+    <>
+      {/* Header 자리 */}
+      <AppRoutes />
+      {/* Footer 자리 */}
+    </>
+  );
+}
+
+export default App;
